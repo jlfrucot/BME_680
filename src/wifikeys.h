f6a38824a7becfa33b38138964bb0b91daceec87
jlfrucot <frucot.jeanlouis@free.fr>
2021-04-23 17:48:28 +0200

    Premier commit

---
+++ src/wifikeys.h
@@ -0,0 +1,5 @@
+const char *ssid =     "Atelier_Max";         // Put your SSID here
+const char *password = "40791685";      // Put your PASSWORD here
+IPAddress deviceLocaleIP(192,168,1,103);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
